f733e7bab882d5ccc2a703dbb8908f1dbd3727e6
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2024-04-05 16:40:34 +0300

    Add homework6

---
+++ homework6/hom6_task1.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import random
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+
+def print_rand(min_val, max_val):
+    num=random.randint(min_val, max_val)
+    num2=num*num
+    print(num,num2)
+
+dag=DAG('hom6_dag', description='Homework6 dag',
+          schedule_interval='* * * * *',
+          start_date=datetime(2023, 1, 1),
+          catchup=False)
+
+bash_random_operator=BashOperator(
+    task_id='bash_random_task',
+    bash_command='echo $RANDOM',
+    dag=dag
+    )
+#python_random_operator=PythonOperator(
+#    task_id='python_random_task',
+#    python_callable=print_rand,
+#    op_args=[0,10],
+#    dag=dag
+#    )
+#python_random_operator=PythonOperator(
+#    task_id='python_random_task',
+#    python_callable=print_rand,
+#    op_kwargs={'min_val':0,'max_val':10},
+#    dag=dag
+#    )
+python_random_operator=PythonOperator(
+    task_id='python_random_task',
+    python_callable=lambda:print_rand(0,10),
+    dag=dag
+    )
+#hello_file_operator=BashOperator(task_id='hello_file_task',bash_command='scripts/file1.sh',dag=dag)
+
+bash_random_operator >> python_random_operator
